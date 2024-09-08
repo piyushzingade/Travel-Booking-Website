604a10d1a784bfe1bd7e4a094a9be7cbffc86bc5
Piyushzingade <piyushzingade@gmail.com>
2024-09-08 18:55:57 +0530

    Added data to the Database and endpoint to it

--- backend/model/TravelPackageModel.js
+++ backend/model/TravelPackageModel.js
@@ -2,6 +2,6 @@ const { model} = require("mongoose");
 
 const { TravelPackageSchema } = require("../schemas/TravelPackageSchema")
 
-const TravelPackageModel = new model("Travel" , TravelPackageSchema)
+const TravelPackageModel = new model("TravelPackage" , TravelPackageSchema)
 
 module.exports = {TravelPackageModel} ; 
