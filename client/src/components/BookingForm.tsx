bdbe21a91f60d164dd1fcf93b3937023a523b12c
Piyushzingade <piyushzingade@gmail.com>
2024-09-09 10:47:00 +0530

    Updated Booking component

--- client/src/components/BookingForm.tsx
+++ client/src/components/BookingForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const BookingForm: React.FC = () => {
+export default function BookingForm(){
   const { state } = useLocation();
   const { pkg, selectedDate } = state || {};
 
@@ -108,4 +108,3 @@ const BookingForm: React.FC = () => {
   );
 };
 
-export default BookingForm;
