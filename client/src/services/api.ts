453a86767d6269bc79a438704043acadaa6b1d08
Piyushzingade <140975601+piyushzingade@users.noreply.github.com>
2024-09-12 00:34:48 +0530

    Update api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export const getPackages = async () => {
   try {
     // Make a GET request to the API endpoint
-    const response = await axios.get("http://localhost:3002/allPackages");
+    const response = await axios.get("https://travel-booking-website-backend-1.onrender.com");
 
     // Return the data from the response
     return response.data;
