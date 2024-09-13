71a3d08daa7a9885d83dd5a0e5334208311236de
Piyushzingade <140975601+piyushzingade@users.noreply.github.com>
2024-09-13 11:12:19 +0530

    Update api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,18 +1,12 @@
+
 import axios from "axios";
 
-// Function to fetch travel packages from the API
 export const getPackages = async () => {
   try {
-    // Make a GET request to the API endpoint
-    const response = await axios.get("https://localhost:3002/allPackages");
-
-    // Return the data from the response
+    const response = await axios.get("http://localhost:3002/allPackages");
     return response.data;
   } catch (error) {
-    // Log the error to the console for debugging
     console.error("Error fetching packages:", error);
-
-    // Rethrow the error to be handled by the calling code
     throw error;
   }
 };
